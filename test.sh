3cc8e96c3427c67f017388621297400c6ea3218b
Cloud User <ec2-user@ip-172-31-12-42.ap-south-1.compute.internal>
2019-12-04 15:53:52 +0000

    test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,6 @@
-git
+mkdir -p ~/.venv/megabank_api
+python3 -m venv ~/.venv/megabank_api
+source ~/.venv/megabank_api/bin/activate
+python3 -m pip install -r requirements.txt
+nohup python3 -m megabank_rest_api &
+
